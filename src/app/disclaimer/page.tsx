73d95149bdc9977cba6cba941b57456475b5b93e
pillagowthamvenkatesh <sepl.gowthamvenkatesh@gmail.com>
2024-08-08 00:22:07 +0530

    disclaimer done

--- src/app/disclaimer/page.tsx
+++ src/app/disclaimer/page.tsx
@@ -97,75 +97,78 @@ const PrivacyPolicy = () => {
 
       {/* third block */}
       <div className="md:w-[85%] mx-auto max-md:px-10 py-10">
-      <div className="flex justify-center font-bold text-3xl mb-6 underline">
-        DISCLAIMER
+        <div className="flex justify-center font-bold text-3xl mb-6 underline">
+          DISCLAIMER
+        </div>
+        <div className="space-y-4 text-sm font-bold">
+          <p>
+            Kjlemon app is an online dating and matchmaking platform. It does not offer escort listings.
+          </p>
+          <p>
+            Kjlemon's services are provided "as is" and we do not make, and cannot make, any representations about the content or features of our services or member content.
+          </p>
+          <p>
+            Kjlemon provides our services on an "as is" and "as available" basis and to the extent permitted by applicable law, grants no warranties of any kind, whether express, implied, statutory or otherwise with respect to our services (including all content contained therein), including, without limitation, any implied warranties of satisfactory quality, merchantability, fitness for a particular purpose or non-infringement. Kjlemon does not represent or warrant that
+          </p>
+          <p>
+            (a) our services will be uninterrupted, secure, or error free,
+          </p>
+          <p>
+            (b) any defects or errors in our services will be discovered or corrected, or
+          </p>
+          <p>
+            (c) that any content or information you obtain on or through our services will be accurate, complete, current or appropriate for your purposes. Furthermore, Kjlemon makes no guarantees as to the number of active users at any time; users' ability or desire to communicate with or meet you, or the ultimate compatibility with or conduct by users you meet through the services.
+          </p>
+          <p>
+            Kjlemon app does not promote nudity. All the users are advised not to upload any obscene pictures.
+          </p>
+          <p>
+            All the users shall be 18 years and above.
+          </p>
+          <p>
+            All users shall be required to open account in order to use the service, chat, call and video call.
+          </p>
+          <p>
+            The app will have two kinds of accounts: basic and premium account.
+          </p>
+          <p>
+            The free account, a user will not pay but can communicate with other users through unlimited chat , and for the basic and premium account, users will be required to subscribe in order to enjoy premium features , receiving gifts from other users, can see when another user read my messages. Video call will be included in premium subscriptions
+          </p>
+          {/* <p>
+            The basic account, a user will not pay but can chat and communicate with others users, and for the premium account, users will be required to subscribe in order to enjoy premium features , include, video call , receiving gifts from others users, can see when another user read my messages.
+          </p> */}
+          <p>
+            Users can create an account by using Apple account, Facebook account or google account, will have google analytics and google location engine or tool.
+          </p>
+          <p>
+            Please note that KJLEMON app neither authenticates any information of users nor it is responsible for any incorrect or inaccurate information provided by the users. Hence, all the users are strongly advised to verify the information and credentials as posted by other users before taking a final decision.
+          </p>
+          <p>
+            KJLEMON app is committed to provide a smooth, trouble-free and safe matchmaking experience to its users by following the industry standard procedures.
+          </p>
+          <p>
+            However, KJLEMON app is not responsible for any technical malfunctioning related to computer, internet services and telephone network, including problems in connecting with the customer support team, failure in email delivery or any problem in browsing the app.
+          </p>
+          <p>
+            Also, KJLEMON app doesn't warranty that the app is free from viruses or any other infection.
+          </p>
+          <p>
+            The profiles listed on KJLEMON app should not be taken as offered or suggested or recommended by the app. Thus, KJLEMON app shall not be responsible for any direct, indirect, special, incidental or consequential damages arising out of the use in the services provided by the app.
+          </p>
+          <p>
+            Under no circumstances, shall KJLEMON app be liable to pay for any indirect, special, incidental, consequential, punitive or exemplary losses or damages to the users arising out of using KJLEMON app.
+          </p>
+          <p>
+            KJLEMON app reserves all the rights to add, delete or modify the contents, presentation and format of the information provided at any time without giving any prior notice.
+          </p>
+          <p>
+            All the users need to refer and agree to the privacy policy and terms of use while accessing the app.
+          </p>
+          <p>
+            KJLEMON app may accept advertisements for display provided by the third parties. It is the user's responsibility to check the authenticity of these advertisements and KJLEMON app will not be responsible for any direct or indirect loss arising to users as a result of using the products or services displayed in the advertisements. Also, the third party advertisers need to ensure that their material complies with all the USA and International laws and regulations. KJLEMON app reserves all the rights to suspend any advertisement at any point of time.
+          </p>
+        </div>
       </div>
-      <div className="space-y-4 text-sm font-bold">
-        <p>
-          Kjlemon app is an online dating and matchmaking platform. It does not offer escort listings.
-        </p>
-        <p>
-          Kjlemon's services are provided "as is" and we do not make, and cannot make, any representations about the content or features of our services or member content.
-        </p>
-        <p>
-          Kjlemon provides our services on an "as is" and "as available" basis and to the extent permitted by applicable law, grants no warranties of any kind, whether express, implied, statutory or otherwise with respect to our services (including all content contained therein), including, without limitation, any implied warranties of satisfactory quality, merchantability, fitness for a particular purpose or non-infringement. Kjlemon does not represent or warrant that
-        </p>
-        <p>
-          (a) our services will be uninterrupted, secure, or error free,
-        </p>
-        <p>
-          (b) any defects or errors in our services will be discovered or corrected, or
-        </p>
-        <p>
-          (c) that any content or information you obtain on or through our services will be accurate, complete, current or appropriate for your purposes. Furthermore, Kjlemon makes no guarantees as to the number of active users at any time; users' ability or desire to communicate with or meet you, or the ultimate compatibility with or conduct by users you meet through the services.
-        </p>
-        <p>
-          Kjlemon app does not promote nudity. All the users are advised not to upload any obscene pictures.
-        </p>
-        <p>
-          All the users shall be 18 years and above.
-        </p>
-        <p>
-          All users shall be required to open account in order to use the service, chat, call and video call.
-        </p>
-        <p>
-          The app will have two kinds of accounts: basic and premium account.
-        </p>
-        <p>
-          The basic account, a user will not pay but can chat and communicate with others users, and for the premium account, users will be required to subscribe in order to enjoy premium features , include, video call , receiving gifts from others users, can see when another user read my messages.
-        </p>
-        <p>
-          Users can create an account by using Apple account, Facebook account or google account, will have google analytics and google location engine or tool.
-        </p>
-        <p>
-          Please note that KJLEMON app neither authenticates any information of users nor it is responsible for any incorrect or inaccurate information provided by the users. Hence, all the users are strongly advised to verify the information and credentials as posted by other users before taking a final decision.
-        </p>
-        <p>
-          KJLEMON app is committed to provide a smooth, trouble-free and safe matchmaking experience to its users by following the industry standard procedures.
-        </p>
-        <p>
-          However, KJLEMON app is not responsible for any technical malfunctioning related to computer, internet services and telephone network, including problems in connecting with the customer support team, failure in email delivery or any problem in browsing the app.
-        </p>
-        <p>
-          Also, KJLEMON app doesn't warranty that the app is free from viruses or any other infection.
-        </p>
-        <p>
-          The profiles listed on KJLEMON app should not be taken as offered or suggested or recommended by the app. Thus, KJLEMON app shall not be responsible for any direct, indirect, special, incidental or consequential damages arising out of the use in the services provided by the app.
-        </p>
-        <p>
-          Under no circumstances, shall KJLEMON app be liable to pay for any indirect, special, incidental, consequential, punitive or exemplary losses or damages to the users arising out of using KJLEMON app.
-        </p>
-        <p>
-          KJLEMON app reserves all the rights to add, delete or modify the contents, presentation and format of the information provided at any time without giving any prior notice.
-        </p>
-        <p>
-          All the users need to refer and agree to the privacy policy and terms of use while accessing the app.
-        </p>
-        <p>
-          KJLEMON app may accept advertisements for display provided by the third parties. It is the user's responsibility to check the authenticity of these advertisements and KJLEMON app will not be responsible for any direct or indirect loss arising to users as a result of using the products or services displayed in the advertisements. Also, the third party advertisers need to ensure that their material complies with all the USA and International laws and regulations. KJLEMON app reserves all the rights to suspend any advertisement at any point of time.
-        </p>
-      </div>
-    </div>
 
       {/* fourth block */}
       <div className="flex justify-center border-t-2 items-center self-stretch px-24 py-8 w-full bg-black max-md:px-5 max-md:max-w-full">
